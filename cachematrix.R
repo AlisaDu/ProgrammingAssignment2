502ce0e2ed76b121f9a43fb046aabe3af0bbc23a
Alisa Dudinova <alisa.dudinova@ace.ee>
2020-10-23 12:08:03 +0300

    Solution for the second Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## mFile contains two functions to define, calculate and retrieve 
+## special matrix object and it's inverse
 
-## Write a short comment describing this function
+
+## function contains get-set methods for special matrix objects
+## and its inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y){
+        x<<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, 
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## function checks the availability of cached  matrix inverse value.
+## if value not available, calculates the inversion and stores it into object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <-x$getinverse()
+    if(!is.null(inv)){
+        message ("cached value available: ")
+        return(inv)
+    } 
+    inv <- solve(x$get())
+    x$setinverse(inv)
+    inv
+    
 }
